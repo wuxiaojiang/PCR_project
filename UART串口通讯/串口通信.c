c88e70a22d36ebbc39982b0edf23589783b0be32
wuxiaojiang <717189544@qq.com>
2015-04-13 22:59:50 +0800

    holmes

---
+++ UART串口通讯/串口通信.c
@@ -0,0 +1,25 @@
+#include<reg52.h>
+#include<uart_com_temp.h>
+
+sbit P20=P2^0;
+sbit P10=P1^0;
+
+void delay(unsigned int nN);
+
+void main()
+{
+	//unsigned char chRcv;
+	//unsigned int i;
+	//unsigned int  t;
+	unsigned int cs = 97;
+	InitUartComm(12,9600);
+    Send_Comm(cs);
+	delay(500);
+}
+void delay(unsigned int nN)
+{
+	unsigned int a,b,c;
+	for(a=0;a<nN;a++)
+		for(b=0;b<50;b++)
+			for(c=0;c<50;c++);
+} 
